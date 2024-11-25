f731e8a28107e02f9390e0d717606f2801f829b2
SantVent <gabrielventura920@gmail.com>
2024-11-25 17:34:07 -0500

    0.1version

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+// Este codigo javascript es utilizado para mostrar el menu en dispositivos moviles 
+document.getElementById("btn-menu").addEventListener("click", mostrar_menu);
+
+document.getElementById("backmenu").addEventListener("click", ocultar_menu);
+
+nav = document.getElementById("menu");
+background_menu = document.getElementById("backmenu");
+
+function mostrar_menu(){
+
+    nav.style.right = "0px";
+    background_menu.style.display = "block";
+}
+
+function ocultar_menu(){
+
+    nav.style.right = "-250px";
+    background_menu.style.display = "none";
+}
